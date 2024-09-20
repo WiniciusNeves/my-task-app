36ed85a1a2ea1b0877def7f70e372b947dbb960b
Winicius-Neves <winiciusneves2004@hotmail.com>
2024-09-20 14:07:47 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,52 @@
-"use client"; // Adicione esta linha no topo do arquivo
+"use client";
 
 import styles from "./login.module.scss";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Login() {
-    const [username, setUsername] = useState("");
+    const [username, setUsername] = useState<string>("");
     const router = useRouter();
 
     const handleLogin = () => {
-        if (username) {
-            localStorage.setItem("username", username);
-            router.push("/tasks");
+        if (!username) {
+            return alert("Por favor, insira seu nome de usuário");
         }
+
+        localStorage.setItem("username", username);
+        router.push("/tasks");
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        handleLogin();
     };
 
     return (
-        <div className={styles.container}>
-            <h1>Login</h1>
-            <input
-                type="text"
-                placeholder="Digite seu nome"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className={styles.input}
-            />
-            <button onClick={handleLogin} className={styles.button}>
-                Entrar
-            </button>
-        </div>
+        <form className={styles.container} onSubmit={handleSubmit}>
+            <div className={styles["logo-container"]}>
+                <Image
+                    src="/images/logo.png"
+                    alt="Logotipo da aplicação"
+                    width={150}
+                    height={36}
+                />
+            </div>
+            <div className={styles["form-container"]}>
+                <h1>Bem-vindo de volta!</h1>
+                <label htmlFor="username">Nome de usuário</label>
+                <input
+                    type="text"
+                    id="username"
+                    placeholder="Insira seu nome de usuário"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </div>
+            <div className={styles["button-container"]}>
+                <button type="submit">Entrar</button>
+            </div>
+        </form>
     );
 }
--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,158 +1,158 @@
-'use client';
-import { useEffect, useState } from 'react';
-import { FaTrash } from 'react-icons/fa';
-import Header from "@/app/components/template/Header";
-import styles from './tasks.module.scss';
+    'use client';
+    import { useEffect, useState } from 'react';
+    import { FaTrash } from 'react-icons/fa';
+    import Header from "@/app/components/template/Header";
+    import styles from './tasks.module.scss';
 
-interface Task {
-    id: number;
-    title: string;
-    completed: boolean;
-}
+    interface Task {
+        id: number;
+        title: string;
+        completed: boolean;
+    }
 
-export default function Tasks() {
-    const [tasks, setTasks] = useState<Task[]>([]);
-    const [showModal, setShowModal] = useState<boolean>(false);
-    const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
-    const [selectedTaskId, setSelectedTaskId] = useState<number | null>(null);
-    const [newTaskTitle, setNewTaskTitle] = useState<string>('');
+    export default function Tasks() {
+        const [tasks, setTasks] = useState<Task[]>([]);
+        const [showModal, setShowModal] = useState<boolean>(false);
+        const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
+        const [selectedTaskId, setSelectedTaskId] = useState<number | null>(null);
+        const [newTaskTitle, setNewTaskTitle] = useState<string>('');
 
-    useEffect(() => {
-        const storedTasks = localStorage.getItem('tasks');
-        if (storedTasks) {
-            setTasks(JSON.parse(storedTasks));
-        }
-    }, []);
+        useEffect(() => {
+            const storedTasks = localStorage.getItem('tasks');
+            if (storedTasks) {
+                setTasks(JSON.parse(storedTasks));
+            }
+        }, []);
 
-    useEffect(() => {
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }, [tasks]);
+        useEffect(() => {
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+        }, [tasks]);
 
-    const addTask = (title: string) => {
-        const newTask: Task = { id: tasks.length + 1, title, completed: false };
-        setTasks([...tasks, newTask]);
-        setShowModal(false);
-        setNewTaskTitle('');
-    };
+        const addTask = (title: string) => {
+            const newTask: Task = { id: tasks.length + 1, title, completed: false };
+            setTasks([...tasks, newTask]);
+            setShowModal(false);
+            setNewTaskTitle('');
+        };
 
-    const toggleTaskCompletion = (id: number) => {
-        setTasks(
-            tasks.map(task =>
-                task.id === id ? { ...task, completed: !task.completed } : task
-            )
-        );
-    };
+        const toggleTaskCompletion = (id: number) => {
+            setTasks(
+                tasks.map(task =>
+                    task.id === id ? { ...task, completed: !task.completed } : task
+                )
+            );
+        };
 
-    const handleDeleteTask = () => {
-        if (selectedTaskId !== null) {
-            setTasks(tasks.filter(task => task.id !== selectedTaskId));
-            setShowDeleteModal(false);
-            setSelectedTaskId(null);
-        }
-    };
+        const handleDeleteTask = () => {
+            if (selectedTaskId !== null) {
+                setTasks(tasks.filter(task => task.id !== selectedTaskId));
+                setShowDeleteModal(false);
+                setSelectedTaskId(null);
+            }
+        };
 
-    const handleOpenDeleteModal = (id: number) => {
-        setSelectedTaskId(id);
-        setShowDeleteModal(true);
-    };
+        const handleOpenDeleteModal = (id: number) => {
+            setSelectedTaskId(id);
+            setShowDeleteModal(true);
+        };
 
-    return (
-        <main>
-            <Header />
-            <div className={styles['tasks-container']}>
-                <h1 className={styles['title']}>Sua Lista de Tarefas</h1>
-                <ul>
-                    {tasks.filter(task => !task.completed).map(task => (
-                        <li key={task.id}>
-                            <input
-                                type="checkbox"
-                                checked={task.completed}
-                                onChange={() => toggleTaskCompletion(task.id)}
-                            />
-                            <span>{task.title}</span>
-                            <button
-                                className={styles['delete-button']}
-                                onClick={() => handleOpenDeleteModal(task.id)}
-                            >
-                                <FaTrash />
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-
-                <h2 className={styles['title']}>Tarefas finalizadas</h2>
-                <ul className={styles['completed-tasks']}>
-                    {tasks.filter(task => task.completed).map(task => (
-                        <li key={task.id}>
-                            <input
-                                type="checkbox"
-                                checked={task.completed}
-                                onChange={() => toggleTaskCompletion(task.id)}
-                            />
-                            <span className={styles.completed}>
-                                {task.title}
-                            </span>
-                            <button
-                                className={styles['delete-button']}
-                                onClick={() => handleOpenDeleteModal(task.id)}
-                            >
-                                <FaTrash />
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-
-                {showModal && (
-                    <div className={styles['modal']}>
-                        <div className={styles['modal-content']}>
-                            <h2>Nova Tarefa</h2>
-                            <form onSubmit={(e) => {
-                                e.preventDefault();
-                                addTask(newTaskTitle);
-                            }}>
-                                <label htmlFor="new-task">Título</label>
+        return (
+            <main>
+                <Header />
+                <div className={styles['tasks-container']}>
+                    <h1 className={styles['title']}>Sua Lista de Tarefas</h1>
+                    <ul>
+                        {tasks.filter(task => !task.completed).map(task => (
+                            <li key={task.id}>
                                 <input
-                                    id="new-task"
-                                    type="text"
-                                    value={newTaskTitle}
-                                    onChange={(e) => setNewTaskTitle(e.target.value)}
-                                    placeholder="Nova tarefa"
-                                    required
+                                    type="checkbox"
+                                    checked={task.completed}
+                                    onChange={() => toggleTaskCompletion(task.id)}
                                 />
-                                <div className={styles['button-group']}>
-                                    <button
-                                        type="button"
-                                        onClick={() => setShowModal(false)}
-                                        className={`${styles['button']} ${styles['cancel-button']}`}>
-                                        Cancelar
-                                    </button>
-                                    <button
-                                        type="submit"
-                                        className={`${styles['button']} ${styles['add-button']}`}>
-                                        Adicionar
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                )}
+                                <span>{task.title}</span>
+                                <button
+                                    className={styles['delete-button']}
+                                    onClick={() => handleOpenDeleteModal(task.id)}
+                                >
+                                    <FaTrash />
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
 
-                {showDeleteModal && (
-                    <div className={styles['modal']}>
-                        <div className={styles['modal-content']}>
-                            <h2>Deletar Tarefa</h2>
-                            <p className={styles['delete-text']}>Tem certeza que você deseja deletar essa tarefa?</p>
-                            <div className={styles['button-group']}>
-                                <button onClick={() => setShowDeleteModal(false)} className={`${styles['button']} ${styles['cancel-button']}`}>Cancelar</button>
-                                <button onClick={handleDeleteTask} className={`${styles['button']} ${styles['delete-button']}`}>Deletar</button>
+                    <h2 className={styles['title']}>Tarefas finalizadas</h2>
+                    <ul className={styles['completed-tasks']}>
+                        {tasks.filter(task => task.completed).map(task => (
+                            <li key={task.id}>
+                                <input
+                                    type="checkbox"
+                                    checked={task.completed}
+                                    onChange={() => toggleTaskCompletion(task.id)}
+                                />
+                                <span className={styles.completed}>
+                                    {task.title}
+                                </span>
+                                <button
+                                    className={styles['delete-button']}
+                                    onClick={() => handleOpenDeleteModal(task.id)}
+                                >
+                                    <FaTrash />
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+
+                    {showModal && (
+                        <div className={styles['modal']}>
+                            <div className={styles['modal-content']}>
+                                <h2>Nova Tarefa</h2>
+                                <form onSubmit={(e) => {
+                                    e.preventDefault();
+                                    addTask(newTaskTitle);
+                                }}>
+                                    <label htmlFor="new-task">Título</label>
+                                    <input
+                                        id="new-task"
+                                        type="text"
+                                        value={newTaskTitle}
+                                        onChange={(e) => setNewTaskTitle(e.target.value)}
+                                        placeholder="Nova tarefa"
+                                        required
+                                    />
+                                    <div className={styles['button-group']}>
+                                        <button
+                                            type="button"
+                                            onClick={() => setShowModal(false)}
+                                            className={`${styles['button']} ${styles['cancel-button']}`}>
+                                            Cancelar
+                                        </button>
+                                        <button
+                                            type="submit"
+                                            className={`${styles['button']} ${styles['add-button']}`}>
+                                            Adicionar
+                                        </button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
-                    </div>
-                )}
-            </div>
-            <div className={styles['add-task']}>
-                <button onClick={() => setShowModal(true)}>Adicionar Tarefa</button>
-            </div>
-        </main>
-    );
-}
+                    )}
+
+                    {showDeleteModal && (
+                        <div className={styles['modal']}>
+                            <div className={styles['modal-content']}>
+                                <h2>Deletar Tarefa</h2>
+                                <p className={styles['delete-text']}>Tem certeza que você deseja deletar essa tarefa?</p>
+                                <div className={styles['button-group']}>
+                                    <button onClick={() => setShowDeleteModal(false)} className={`${styles['button']} ${styles['cancel-button']}`}>Cancelar</button>
+                                    <button onClick={handleDeleteTask} className={`${styles['button']} ${styles['delete-button']}`}>Deletar</button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+                <div className={styles['add-task']}>
+                    <button onClick={() => setShowModal(true)}>Adicionar Tarefa</button>
+                </div>
+            </main>
+        );
+    }
